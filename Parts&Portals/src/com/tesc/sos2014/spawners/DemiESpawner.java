d2fd0926427b24664c3921b92d1c3736826d6537
Daniel Cochran <daniel@blusteel.com>
2014-05-22 10:50:22 -0700

    Code Update

--- Parts&Portals/src/com/tesc/sos2014/spawners/DemiESpawner.java
+++ Parts&Portals/src/com/tesc/sos2014/spawners/DemiESpawner.java
@@ -1,10 +1,67 @@
 package com.tesc.sos2014.spawners;
 
+import org.andengine.engine.camera.Camera;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.extension.physics.box2d.PhysicsFactory;
+import org.andengine.extension.physics.box2d.PhysicsWorld;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+
+import android.util.Log;
+
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.tesc.sos2014.managers.ResourcesManager;
+import com.tesc.sos2014.objects.DemiEnemy;
+import com.tesc.sos2014.partsportals.MainGameEngineActivity;
+import com.tesc.sos2014.pools.DemiEnemyPool;
+import com.tesc.sos2014.scenes.GameScene;
+
 public class DemiESpawner
 {
+	public float x;
+	public float y;
+	public Sprite s;
+	public int groupNum = 5;
 
-	public DemiESpawner()
+	public DemiESpawner(float pX, float pY, VertexBufferObjectManager vbo, Camera camera, PhysicsWorld physicsWorld)
 	{
 
+		this.x = pX;
+		this.y = pY;
+		s= new Sprite(pX, pY, ResourcesManager.getInstance().bullet, vbo);
+		s.setScale(0.000001f);
+		s.setVisible(false);
+		Body body = PhysicsFactory.createBoxBody(physicsWorld, s, BodyType.StaticBody, PhysicsFactory.createFixtureDef(0, 0, 0));
+		
+	}
+	
+	public void SpawnE()
+	{
+		for(int i =0; i <= groupNum; i++)
+		{
+			GameScene scene = (GameScene) MainGameEngineActivity.getSharedInstance().mCurrentScene;
+			
+			
+
+			DemiEnemy DE = DemiEnemyPool.sharedDemiEnemyPool().obtainPoolItem();
+			
+
+			DE.aSprite.setPosition(x, y);
+			
+			DE.aSprite.setVisible(true);
+		
+			DE.aSprite.detachSelf();
+			
+			DE.aSprite.setSize(40, 40);
+			DE.animateMe();
+			
+			scene.demiEnemyCount++;
+
+			scene.attachDemiEnemy(DE);
+			
+			//Get Enemy from Pool
+			//Attach to Scene
+			//Add to enemyList in GameScene
+		}
 	}
 }
--- Parts&Portals/src/com/tesc/sos2014/spawners/DemiESpawnerT.java
+++ Parts&Portals/src/com/tesc/sos2014/spawners/DemiESpawnerT.java
@@ -1,6 +1,26 @@
 package com.tesc.sos2014.spawners;
 
+import org.andengine.engine.camera.Camera;
+import org.andengine.extension.physics.box2d.PhysicsWorld;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+
 public class DemiESpawnerT extends DemiESpawner
 {
+	public int time = 0;
+
+
+
+
+	public DemiESpawnerT(float pX, float pY, VertexBufferObjectManager vbo, Camera camera, PhysicsWorld physicsWorld)
+		{
+			super(pX, pY, vbo, camera, physicsWorld);
+			
+		}
+	
+	public void setTimer(int t)
+	{
+		this.time = t;
+		
+	}
 
 }
